4c8c04e0e1d19c3bed0215ccf808850baad543eb
FengShuai123456 <2444904711@qq.com>
2019-03-18 11:31:33 +0800

    提交新创建的项目

---
+++ src/main/java/com/hr/sm_p/SmPApplication.java
@@ -0,0 +1,13 @@
+package com.hr.sm_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmPApplication.class, args);
+    }
+
+}
